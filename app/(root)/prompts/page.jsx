7a676e68ade6738c6d87e5ce4daf4e21201e53e5
Somkene Ojukwu <somkeneoj@gmail.com>
2024-07-29 05:06:11 +0100

    g:Form design applied

--- app/(root)/prompts/page.jsx
+++ app/(root)/prompts/page.jsx
@@ -21,37 +21,46 @@ const PromptPage = () => {
 
 
   return (
-    <div>
-      <h1>Prompts</h1>
-      <ul>
+    <div className="cursor-pointer mt-40 mb-4 container mx-auto max-w-lg border border-gray-300 rounded-lg p-0">
+      <ul className="flex flex-col ">
         {prompts.map((prompt) => (
-          <li key={prompt._id}>
-            <h2>{prompt.prompt}</h2>
-            <p>Site: {prompt.site}</p>
-            {prompt.img ? (
-              <Image
-                alt="prompt-image"
-                src={prompt.img}
-                width={200}
-                height={200}
-              />
-            ) : null}
-            <p className="text-blue-500 hover:underline" >
-              <Link href={`/profile/${prompt.author.username}/`}>
-                username: {prompt.author.username}
+          <li key={prompt._id} className="flex p-4 border-b border-gray-200">
+            <div className="flex flex-col flex-grow">
+              <div className="flex items-center mb-2">
+                <Link href={`/profile/${prompt.author.username}/`}>
+                  <Image
+                    src={prompt.author.photo} // Replace with actual avatar source
+                    alt="profilepic"
+                    width={100}
+                    height={100}
+                    className="w-8 h-8 rounded-full mr-2"
+                  />
+                </Link>
+                <p className="text-black">
+                  {prompt.author.username} <span className="text-gray-500">@{prompt.author.username}.</span>  <span className="text-gray-500 text-sm">{new Date(prompt.createdAt).toLocaleDateString()}</span>
+                </p>
+              </div>
+
+              <h4 className="text-md font-normal mb-2">{prompt.prompt}</h4>
+              {prompt.img ? (
+                <Image
+                  alt="prompt-image"
+                  src={prompt.img}
+                  width={200}
+                  height={200}
+                  layout="responsive"
+                  className="w-full rounded-lg mt-4 mb-2"
+                />
+              ) : null}
+              <Link className="text-blue-500 hover:underline text-sm mt-2" href={`/prompts/${prompt._id}`}>
+                View More
               </Link>
-            </p>
-            <p>Author: {prompt.author.email}</p>
-            <p>Created At: {new Date(prompt.createdAt).toLocaleDateString()}</p>
-            {/* Use Link component for client-side navigation */}
-            < Link className="text-blue-500 hover:underline" href={`/prompts/${prompt._id}`}>
-              View More
-            </Link>
+            </div>
           </li>
-        ))
-        }
-      </ul >
-    </div >
+        ))}
+      </ul>
+    </div>
+
   );
 };
 
