70e64537ea98d536d064b4a7bed85ba6024da709
Somkene Ojukwu <somkeneoj@gmail.com>
2024-08-19 00:54:22 +0100

    12:test

--- components/shared/navbar.jsx
+++ components/shared/navbar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
                         <div className="flex items-center justify-between">
                             {/* Company logo in mobile menu */}
                             <Link href="/" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
+                                <span className="sr-only">your Company</span>
                                 <img
                                     alt="Company Logo"
                                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
